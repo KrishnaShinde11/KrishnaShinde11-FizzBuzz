7fda3b49b3bdf60debc81283d84daf531078b98a
Kevin Childs <kchilds2020@gmail.com>
2023-01-03 13:49:53 -0600

    first commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,20 @@
+
+public class FizzBuzz {
+    /**
+     * Return "Fizz" if a number n is divisible by 3, "Buzz" if divisible by 5, and "FizzBuzz" if divisible by both.
+     *
+     * This is coding problem that will make use of the modulus (%) operator, which can help us calculate if a number
+     * is perfectly divisible by another number.
+     * The modulus operator returns the remainder after a division (eg 8%3=2).
+     *
+     * If you try to copy answers from the internet, you will probably get this challenge wrong. The premise is
+     * slightly simpler than the classic fizzbuzz problem.
+     *
+     * @param n the number that will determine the fizzbuzz result.
+     * @return if a number is divisible by 3, return 'Fizz'. if a number is divisible by 5, return 'Buzz'. If
+     * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
+     */
+    public String FizzBuzzGen(int n){
+        return "";
+    }
+}
